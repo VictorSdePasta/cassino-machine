bbd7cad9c2cf3c4da2c54a565aa5ba1f0097800f
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-09-17 23:14:15 -0300

    adicionar arquivo com declaração das funções de conversão de bases manuais e implementação da conversão manual de binário para decimal

---
+++ js/conversoes_bases.js
@@ -0,0 +1,54 @@
+function converterBinarioParaDecimal(binario) {
+    let decimal = 0;
+
+    for (i = 0; i < binario.length; i++) {
+        decimal = decimal * 2 + binario[i];
+    }
+
+    return decimal;
+}
+
+function converterBinarioParaOctal(binario) {
+    // Implementar depois
+}
+
+function converterBinarioParaHexadecimal(binario) {
+    // Implementar depois
+}
+
+function converterDecimalParaBinario(decimal) {
+    // Implementar depois
+}
+
+function converterDecimalParaOctal(decimal) {
+    // Implementar depois
+}
+
+function converterDecimalParaHexadecimal(decimal) {
+    // Implementar depois
+}
+
+function converterOctalParaBinario(octal) {
+    // Implementar depois
+}
+
+function converterOctalParaDecimal(octal) {
+    // Implementar depois
+}
+
+function converterOctalParaHexadecimal(octal) {
+    // Implementar depois
+}
+
+function converterHexadecimalParaBinario(hexadecimal) {
+    // Implementar depois
+}
+
+function converterHexadecimalParaDecimal(hexadecimal) {
+    // Implementar depois
+}
+
+function converterHexadecimalParaOctal(hexadecimal) {
+    // Implementar depois
+}
+
