484d85d551942a3d7318939bb8aa1a180e928b0b
VictorSdePasta <victor.delima@sptech.school>
2025-10-01 14:40:05 -0300

    conversao das cartas do conevrsor para o resultado

--- js/cartas.js
+++ js/cartas.js
@@ -1,6 +1,7 @@
-let cartaConversao;
-let chanceEspecial = .05;
-let comeco = false;
+let cartaConversao = ''
+let baseAtual = ''
+let chanceEspecial = .05
+let comeco = false
 let cartas = []
 
 function sacarConversao() {
@@ -48,31 +49,30 @@ function saqueInicial() {
   comeco = true
   let saqueIncicial = Math.ceil(Math.random() * 4 + 1)
   let numero = ''
-  let base = ''
 
   if (saqueIncicial == 1) {
-    base = 'decimal'
+    baseAtual = 'decimal'
     for (let i = 0; i <= 3; i++) {
       numero += Math.ceil(Math.random() * 10 - 1) //Sortear numero de 0 a 9
     }
   } else if (saqueIncicial == 2) {
-    base = 'hexadecimal'
+    baseAtual = 'hexadecimal'
     for (let i = 0; i <= 2; i++) {
       numero += '0123456789ABCDEF'[Math.ceil(Math.random() * 16 - 1)] //Sortear numero de 0 a 15
     }
   } else if (saqueIncicial == 3) {
-    base = 'octal'
+    baseAtual = 'octal'
     for (let i = 0; i <= 3; i++) {
       numero += Math.ceil(Math.random() * 8 - 1) //Sortear numero de 0 a 7
     }
   } else {
-    base = 'binario'
+    baseAtual = 'binario'
     for (let i = 0; i <= 5; i++) {
       numero += Math.ceil(Math.random() * 2 - 1) //Sortear numero de 0 a 1
     }
   }
 
-  transformarCartas(base, numero)
+  transformarCartas(baseAtual, numero)
   let msg = ``
   for (let i = 0; i < cartas.length; i++) {
     conversoes++
@@ -105,7 +105,7 @@ function transformarCartas(base, valor) {
     }
   } else if (base == 'hexadecimal') {
     for (let i = 0; i < valor.length - 1;) {
-      cartas.push(`card_clubs_${valor[i]}_hexa.png`)
+      cartas.push(`card_clubs_${valor[i]}.png`)
       i++
     }
   } else if (base == 'octal') {
@@ -130,12 +130,12 @@ function moverConversao(elementoCarta, carta) {
   let destino = document.getElementById(`divConversor`)
   destino.appendChild(cartaAMover)
 
-  let limiteMaximo = conversoes  
+  let limiteMaximo = conversoes
 
   if (ordemConversao.length < limiteMaximo) {
     ordemConversao.push(carta)
   } else {
-    ordemConversao.shift() 
+    ordemConversao.shift()
     ordemConversao.push(carta)
   }
 
@@ -143,10 +143,58 @@ function moverConversao(elementoCarta, carta) {
 }
 
 function converter() {
-  
+  let numeroAConverter = ''
+  let numeroConvertido
   let msg = ``
   for (let i = 0; i <= ordemConversao.length - 1; i++) {
-    msg += `<div id="campoCartaConvertido${i}"><input type="checkbox" id="cartaConvertida${i}"><label class="labelCarta" for="cartaConvertida${i}" onclick="moverMao('campoCartaConvertido${i}', '${ordemConversao[i]}')"><img src="./css/assets/kenney_playing-cards-pack/PNG/Cards (large)/${ordemConversao[i]}" style="height: 15.98vh"></label></div>`
+    let caracter = ordemConversao[i].length - 5
+    numeroAConverter += `${ordemConversao[i][caracter]}`
   };
+
+  console.log(numeroAConverter + ' numero a converter')
+  console.log(baseAtual + ' base atual')
+
+  let decimalValue = parseInt(numeroAConverter, baseAtual === 'hexadecimal' ? 16 : baseAtual === 'octal' ? 8 : baseAtual === 'binario' ? 2 : 10);
+
+  console.log(decimalValue + ' decimal')
+
+  console.log(cartaConversao + ' cartaConversao')
+  console.log(baseAtual+ ' base')
+  if (cartaConversao == 'decimal') {
+    numeroConvertido = `${decimalValue}`;
+  }
+  if (cartaConversao == 'hexadecimal') {
+    numeroConvertido = `${decimalValue.toString(16).toUpperCase()}`;
+  }
+  if (cartaConversao == 'octal') {
+    numeroConvertido = `${decimalValue.toString(8)}`;
+  }
+  if (cartaConversao == 'binario') {
+    numeroConvertido = `${decimalValue.toString(2)}`;
+  }
+
+  console.log(numeroConvertido + ' numero convertido')
+
+  let novasCartas = []
+
+
+  for (let i = 0; i <= numeroConvertido.length - 1; i++) {
+    if (cartaConversao == 'decimal') {
+      novasCartas.push(`card_spades_${numeroConvertido[i]}.png`)
+    }
+    else if (cartaConversao == 'hexadecimal') {
+      novasCartas.push(`card_clubs_${numeroConvertido[i]}.png`)
+    }
+    else if (cartaConversao == 'octal') {
+      novasCartas.push(`card_diamonds_${numeroConvertido[i]}.png`)
+    }
+    else if (cartaConversao == 'binario') {
+      novasCartas.push(`card_hearts_${numeroConvertido[i]}.png`)
+    }
+  }
+
+  for (let i = 0; i <= numeroConvertido.length - 1; i++) {
+    msg += `<div id="campoCartaConvertido${i}"><input type="checkbox" id="cartaConvertida${i}"><label class="labelCarta" for="cartaConvertida${i}" onclick="moverMao('campoCartaConvertido${i}', '${novasCartas[i]}')"><img src="./css/assets/kenney_playing-cards-pack/PNG/Cards (large)/${novasCartas[i]}" style="height: 15.98vh"></label></div>`
+  }
   document.getElementById('divResultadoConversao').innerHTML = msg
 }
